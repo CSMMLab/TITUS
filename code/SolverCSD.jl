42e7734d2192ea763ec104e70cbb445fcbad79e8
Jonas Kusch <jonas.kusch@kit.edu>
2021-09-15 23:11:41 +0200

    setting up sparse matrix efficient now

--- code/SolverCSD.jl
+++ code/SolverCSD.jl
@@ -115,36 +115,114 @@ struct SolverCSD
         L1y = spzeros(nx*ny,nx*ny);
         L2x = spzeros(nx*ny,nx*ny);
         L2y = spzeros(nx*ny,nx*ny);
+
+        # setup index arrays and values for allocation of stencil matrices
+        II = zeros(3*(nx-2)*(ny-2)); J = zeros(3*(nx-2)*(ny-2)); vals = zeros(3*(nx-2)*(ny-2));
+        counter = -2;
+
         for i = 2:nx-1
             for j = 2:ny-1
+                counter = counter + 3;
                 # x part
                 index = vectorIndex(nx,i,j);
                 indexPlus = vectorIndex(nx,i+1,j);
                 indexMinus = vectorIndex(nx,i-1,j);
-                L1x[index,index] = 2.0/2/settings.dx/density[i,j]; 
+
+                II[counter+1] = index;
+                J[counter+1] = index;
+                vals[counter+1] = 2.0/2/settings.dx/density[i,j]; 
                 if i > 1
-                    L1x[index,indexMinus] = -1/2/settings.dx/density[i-1,j]; 
-                    L2x[index,indexMinus] = -1/2/settings.dx/density[i-1,j]; 
+                    II[counter] = index;
+                    J[counter] = indexMinus;
+                    vals[counter] = -1/2/settings.dx/density[i-1,j];
                 end
                 if i < nx
-                    L1x[index,indexPlus] = -1/2/settings.dx/density[i+1,j]; 
-                    L2x[index,indexPlus] = 1/2/settings.dx/density[i+1,j];
-                end
-
-                # y part
-                indexPlus = vectorIndex(nx,i,j+1);
-                indexMinus = vectorIndex(nx,i,j-1);
-                L1y[index,index] += 2.0/2/settings.dy/density[i,j]; 
-                if j > 1
-                    L1y[index,indexMinus] = -1/2/settings.dy/density[i,j-1]; 
-                    L2y[index,indexMinus] = -1/2/settings.dy/density[i,j-1];
-                end
-                if j < ny
-                    L1y[index,indexPlus] = -1/2/settings.dy/density[i,j+1]; 
-                    L2y[index,indexPlus] = 1/2/settings.dy/density[i,j+1];
+                    II[counter+2] = index;
+                    J[counter+2] = indexPlus;
+                    vals[counter+2] = -1/2/settings.dx/density[i+1,j]; 
                 end
             end
         end
+        L1x = sparse(II,J,vals,nx*ny,nx*ny);
+
+        II .= zeros(3*(nx-2)*(ny-2)); J .= zeros(3*(nx-2)*(ny-2)); vals .= zeros(3*(nx-2)*(ny-2));
+        counter = -2;
+
+        for i = 2:nx-1
+            for j = 2:ny-1
+                counter = counter + 3;
+                # y part
+                index = vectorIndex(nx,i,j);
+                indexPlus = vectorIndex(nx,i,j+1);
+                indexMinus = vectorIndex(nx,i,j-1);
+
+                II[counter+1] = index;
+                J[counter+1] = index;
+                vals[counter+1] = 2.0/2/settings.dy/density[i,j]; 
+
+                if j > 1
+                    II[counter] = index;
+                    J[counter] = indexMinus;
+                    vals[counter] = -1/2/settings.dy/density[i,j-1];
+                end
+                if j < ny
+                    II[counter+2] = index;
+                    J[counter+2] = indexPlus;
+                    vals[counter+2] = -1/2/settings.dy/density[i,j+1]; 
+                end
+            end
+        end
+        L1y = sparse(II,J,vals,nx*ny,nx*ny);
+
+        II = zeros(2*(nx-2)*(ny-2)); J = zeros(2*(nx-2)*(ny-2)); vals = zeros(2*(nx-2)*(ny-2));
+        counter = -1;
+
+        for i = 2:nx-1
+            for j = 2:ny-1
+                counter = counter + 2;
+                # x part
+                index = vectorIndex(nx,i,j);
+                indexPlus = vectorIndex(nx,i+1,j);
+                indexMinus = vectorIndex(nx,i-1,j);
+
+                if i > 1
+                    II[counter] = index;
+                    J[counter] = indexMinus;
+                    vals[counter] = -1/2/settings.dx/density[i-1,j];
+                end
+                if i < nx
+                    II[counter+1] = index;
+                    J[counter+1] = indexPlus;
+                    vals[counter+1] = 1/2/settings.dx/density[i+1,j];
+                end
+            end
+        end
+        L2x = sparse(II,J,vals,nx*ny,nx*ny);
+
+        II .= zeros(2*(nx-2)*(ny-2)); J .= zeros(2*(nx-2)*(ny-2)); vals .= zeros(2*(nx-2)*(ny-2));
+        counter = -1;
+
+        for i = 2:nx-1
+            for j = 2:ny-1
+                counter = counter + 2;
+                # y part
+                index = vectorIndex(nx,i,j);
+                indexPlus = vectorIndex(nx,i,j+1);
+                indexMinus = vectorIndex(nx,i,j-1);
+
+                if j > 1
+                    II[counter] = index;
+                    J[counter] = indexMinus;
+                    vals[counter] = -1/2/settings.dy/density[i,j-1];
+                end
+                if j < ny
+                    II[counter+1] = index;
+                    J[counter+1] = indexPlus;
+                    vals[counter+1] = 1/2/settings.dy/density[i,j+1];
+                end
+            end
+        end
+        L2y = sparse(II,J,vals,nx*ny,nx*ny);
 
         new(x,y,settings,outRhs,gamma,AbsAx,AbsAz,P,mu,w,csd,pn,density,vec(density'),dose,L1x,L1y,L2x,L2y);
     end
@@ -245,9 +323,6 @@ function Solve(obj::SolverCSD)
     energy = obj.csd.eGrid;
     S = obj.csd.S;
 
-    # define density matrix
-    densityInv = Diagonal(1.0 ./obj.density);
-
     # Set up initial condition and store as matrix
     v = SetupIC(obj);
     nx = obj.settings.NCellsX;
@@ -257,6 +332,10 @@ function Solve(obj::SolverCSD)
     for k = 1:N
         u[:,k] = vec(v[:,:,k]);
     end
+
+    # define density matrix
+    densityInv = Diagonal(1.0 ./obj.density);
+    Id = Diagonal(ones(N));
 
     # setup gamma vector (square norm of P) to nomralize
     settings = obj.settings
@@ -315,7 +394,7 @@ function Solve(obj::SolverCSD)
         #uTilde[1,:] .= BCLeft(obj,n);
         #uNew = uTilde .- dE*uTilde*D;
         for j = 1:size(uNew,1)
-            uNew[j,:] = (I + dE*D)\uTilde[j,:];
+            uNew[j,:] = (Id .+ dE*D)\uTilde[j,:];
         end
         
         # update dose
