db02a481a608423d78b0fdb39af7f6211e4cc0eb
piastammer <pia.stammer@kit.edu>
2022-10-27 18:50:26 +0200

    Corrected sign in integration, added formula for nuclear proton-oxygen scatterin and corrected formula for proton-proton scattering

--- code/CSD.jl
+++ code/CSD.jl
@@ -103,16 +103,14 @@ function SigmaAtEnergy(obj::CSD{T}, energy::T) where {T<:AbstractFloat}
         for i = 1:(obj.settings.nPN+1)
             # define Sigma mapping for interpolation at moment i
             if energy<7 .+ obj.settings.eRest
-                print(size(obj.E_Tab))
-                print(size(obj.E_sigmaTab))
                 E2Sigma_pp = LinearInterpolation(obj.E_Tab, obj.sigma_pp[:,i]; extrapolation_bc=Throw())
                 E2Sigma_ce = LinearInterpolation(obj.E_Tab, obj.sigma_ce[:,i]; extrapolation_bc=Throw())
-                y[i] = 2 .* E2Sigma_pp(energy) + E2Sigma_ce(energy);
+                y[i] =  E2Sigma_ce(energy);
             else
                 E2Sigma_O = LinearInterpolation(obj.E_sigmaTab, obj.sigma_tab[:,i]; extrapolation_bc=Throw())
                 E2Sigma_pp = LinearInterpolation(obj.E_Tab, obj.sigma_pp[:,i]; extrapolation_bc=Throw())
                 E2Sigma_ce = LinearInterpolation(obj.E_Tab, obj.sigma_ce[:,i]; extrapolation_bc=Throw())
-                y[i] =  E2Sigma_O(energy) .+ 2 .* E2Sigma_pp(energy) + E2Sigma_ce(energy);
+                y[i] =  1*E2Sigma_O(energy) .+ 2 .* E2Sigma_pp(energy)+ E2Sigma_ce(energy);
             end
         end
     else
